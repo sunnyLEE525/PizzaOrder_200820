6abef297bebfb6ccc427765e48cda8bbc56f22fa
wbh <showmana@naver.com>
2020-08-24 19:59:40 +0900

    닉네임 변경 화면 추가

---
+++ app/src/main/java/kr/co/tjoeun/pizzaorder_200820/EditNicknameActivity.kt
@@ -0,0 +1,20 @@
+package kr.co.tjoeun.pizzaorder_200820
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditNicknameActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_nickname)
+}
+    override fun setUpEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+
+
+}
--- app/src/main/java/kr/co/tjoeun/pizzaorder_200820/fragments/MyProfileFragment.kt
+++ app/src/main/java/kr/co/tjoeun/pizzaorder_200820/fragments/MyProfileFragment.kt
@@ -1,10 +1,13 @@
 package kr.co.tjoeun.pizzaorder_200820.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_my_profile.*
+import kr.co.tjoeun.pizzaorder_200820.EditNicknameActivity
 import kr.co.tjoeun.pizzaorder_200820.R
 
 class MyProfileFragment : Fragment() {
@@ -21,6 +24,10 @@ class MyProfileFragment : Fragment() {
         super.onActivityCreated(savedInstanceState)
 
 //        닉네임 변경 관련 코드 작성 필요
+
+        changeNicknameBtn.setOnClickListener {
+            val myIntent = Intent(context!!, EditNicknameActivity::class.java)
+        }
 
 
     }
