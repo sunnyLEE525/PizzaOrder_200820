bca1ef9307868f3acc383c1425ab7a55055f1cd3
wbh <showmana@naver.com>
2020-08-24 20:12:29 +0900

    닉네임 길이가 5자 이상일 때만 완료 - if 활용

--- app/src/main/java/kr/co/tjoeun/pizzaorder_200820/EditNicknameActivity.kt
+++ app/src/main/java/kr/co/tjoeun/pizzaorder_200820/EditNicknameActivity.kt
@@ -3,6 +3,7 @@ package kr.co.tjoeun.pizzaorder_200820
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_edit_nickname.*
 
 class EditNicknameActivity : BaseActivity() {
@@ -16,10 +17,22 @@ class EditNicknameActivity : BaseActivity() {
 
             val inputNickname = newNicknameEdt.text.toString()
 
-            val resultIntent = Intent()
-            resultIntent.putExtra("nick", inputNickname)
-            setResult(Activity.RESULT_OK, resultIntent)
-            finish()
+//            입력한 닉네임이 5글자 넘어야 이전 화면으로 복귀.
+//            그렇지 않으면 토스트로 "닉네임은 5자 이상이어야 합니다." 문구 출력
+
+            if (inputNickname.length >= 5) {
+                val resultIntent = Intent()
+                resultIntent.putExtra("nick", inputNickname)
+                setResult(Activity.RESULT_OK, resultIntent)
+                finish()
+            }
+            else {
+                Toast.makeText(mContext, "닉네임은 5자 이상이어야 합니다.", Toast.LENGTH_SHORT).show()
+            }
+
+
+
+
         }
 
     }
